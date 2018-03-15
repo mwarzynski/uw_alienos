7146e410ddf1036b86b4567426befdc7906ac11d
Mateusz Warzyński <mathew.warzynski@gmail.com>
2018-03-16 00:46:20 +0100

    Comments about params segment - alienos

--- alienos/include/alienos.h
+++ alienos/include/alienos.h
@@ -1,6 +1,17 @@
+
+// Key presses representation as numbers.
+// Range for ASCII: 0x20 - 0x7e.
 #define KEY_UP 0x80
 #define KEY_LEFT 0x81
 #define KEY_DOWN 0x82
 #define KEY_RIGHT 0x83
+#define KEY_ENTER 0x0
 
+// PT - segment for parameters passed from the operating system.
+// Parameters are always of type int and are passed in binary form (4 bytes, little-endian).
+// The size of the `PT_PARAMS` segment indicates how many parameters a given program
+// needs (`p_memsz / 4`).
+// Before the program starts, the operating system places parameter values in this segment.
+// If the program does not have such a segment, it means that it does not accept parameters.
 #define PT_PARAMS 0x60031337
+
--- alienos/src/emu.c
+++ alienos/src/emu.c
@@ -3,7 +3,12 @@
 #include <alienos.h>
 #include <emu.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        return 127;
+    }
+
     printf("Welcome at AlienOS!\n");
+
     return 0;
 }
